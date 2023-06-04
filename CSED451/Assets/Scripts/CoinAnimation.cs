c72d8bcb530d86be95742cbc26e558d7b0e05c80
Minsu Gong <gmsgms0606@gmail.com>
2023-06-04 22:32:48 +0900

    Add: Coin aggro

---
+++ CSED451/Assets/Scripts/CoinAnimation.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinAnimation : ThrowableAnimation
+{
+    GameObject[] m_enemies;
+    EnemyAggro m_enemyAggro;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Map") || col.gameObject.CompareTag("Enemy"))
+        {
+            m_enemies = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach (GameObject enemy in m_enemies)
+            {
+                m_enemyAggro = enemy.GetComponent<EnemyAggro>();
+                if (m_enemyAggro != null)
+                {
+                    m_enemyAggro.TryAggro(transform.position);
+                }
+            }
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ CSED451/Assets/Scripts/EnemyAggro.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAggro : MonoBehaviour
+{
+    public float aggroDistance = 5.0f;
+    public float aggroTime = 5.0f;
+    public float turnSpeed = 0.01f;
+    WaypointPatrol m_waypointPatrol;
+    bool m_isAggro;
+    Vector3 m_targetPosition;
+    Vector3 m_targetDirection;
+    Quaternion m_rotGoal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_waypointPatrol = GetComponent<WaypointPatrol>();
+        m_isAggro = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_isAggro)
+        {
+            m_targetDirection = (m_targetPosition - transform.position).normalized;
+            m_rotGoal = Quaternion.LookRotation(m_targetDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, m_rotGoal, turnSpeed);
+        }
+    }
+
+    public void TryAggro(Vector3 aggroPos) {
+        if (Vector3.Distance(aggroPos, transform.position) <= aggroDistance)
+        {
+            Aggro(aggroPos);
+        }
+    }
+
+    void Aggro(Vector3 aggroPos) {
+        m_waypointPatrol.isStopped = true;
+        m_isAggro = true;
+        m_targetPosition = aggroPos;
+        StartCoroutine(Delay(aggroTime));
+    }
+    IEnumerator Delay(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        m_waypointPatrol.isStopped = false;
+        m_isAggro = false;
+    }
+}
